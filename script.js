d25f0b09a5e51aebbb9767dd93dab8d0d153c5bc
debug-arun <70369284+debug-arun@users.noreply.github.com>
2020-09-02 12:00:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 // Function expression to select elements.
+//Notification
+
+console.log(Notification.permission);
+  if (Notification.permission === "granted") {
+     alert("we have permission");
+  } else if (Notification.permission !== "denied") {
+     Notification.requestPermission().then(permission => {
+        console.log(permission);
+     });
+  }
+
+//side menu
+// Function expression to select elements.
 const selectElement = (s) => document.querySelector(s);
 
 
@@ -12,3 +25,21 @@ selectElement('.open').addEventListener('click', () => {
 selectElement('.close').addEventListener('click', () => {
   selectElement('.nav-list').classList.remove('active');
 });
+
+
+
+
+
+/*const selectElement = (s) => document.querySelector(s);
+
+
+//Open the menu on click
+selectElement('.open').addEventListener('click', () => {
+  selectElement('.nav-list').classList.add('active');
+});
+
+
+//Close the menu on click
+selectElement('.close').addEventListener('click', () => {
+  selectElement('.nav-list').classList.remove('active');
+});*/
